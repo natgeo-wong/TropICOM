c6e16063a497f5a9e81d2f8eb5830bff67c53ee7
Nathanael Wong <natgeo.wong@outlook.com>
2021-01-19 17:23:44 -0500

    Adding functions to create LSF files for SAM

---
+++ src/samlsf.jl
@@ -0,0 +1,38 @@
+using Printf
+
+lsfinit(nvert::Integer) = zeros(nvert,7)
+
+function lsfprint(flsf::AbstractString,lsf::Array{<:Real,2},p::Real)
+
+    nz = size(lsf,1)
+
+    open(flsf,"w") do io
+        @printf(io,"  z[m] p[mb] tpls[K/s] qls[kg/kg/s] uls_hor vls_hor wls[m/s]\n")
+        @printf(io,"%10.2f, %10d, %10.2f\n",0.00,nz,p)
+    end
+
+    open(flsf,"a") do io
+        for iz = 1 : nz
+            @printf(
+                io,"%16.8f, %16.0f, %16.8e, %16.8e, %16.8f, %16.8f, %16.8f\n",
+                lsf[iz,1],lsf[iz,2],lsf[iz,3],
+                lsf[iz,4],lsf[iz,5],lsf[iz,6],lsf[iz,7]
+            )
+        end
+    end
+
+    open(flsf,"a") do io
+        @printf(io,"%10.2f, %10d, %10.2f\n",10000.00,nz,p)
+    end
+
+    open(flsf,"a") do io
+        for iz = 1 : nz
+            @printf(
+                io,"%16.8f, %16.0f, %16.8e, %16.8e, %16.8f, %16.8f, %16.8f\n",
+                lsf[iz,1],lsf[iz,2],lsf[iz,3],
+                lsf[iz,4],lsf[iz,5],lsf[iz,6],lsf[iz,7]
+            )
+        end
+    end
+
+end
