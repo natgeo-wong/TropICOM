5fd6a60a37ab8f96fce5a01acbc9d7d9896dfd2f
natgeo-wong <natgeo.wong@outlook.com>
2021-08-05 22:40:38 -0400

    Adding for filtered land-sea mask and csfvprcp binning by that

---
+++ notebooks/01c-lsmfiltering.jl
@@ -0,0 +1,182 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a675084e-f638-11eb-2930-b70006b9445c
+begin
+	using Pkg; Pkg.activate()
+	using DrWatson
+	md"Using DrWatson in order to ensure reproducibility between different machines ..."
+end
+
+# ╔═╡ 4e08778c-cd46-4a79-9b6a-96ca6447e975
+begin
+	@quickactivate "TroPrecLS"
+	using DelimitedFiles
+	using GeoRegions
+	using ImageFiltering
+	using NCDatasets
+
+	using ImageShow, PNGFiles
+	using PyCall, LaTeXStrings
+	pplt = pyimport("proplot")
+
+	md"Loading modules for the TroPrecLS project..."
+end
+
+# ╔═╡ faa41f45-5a58-4bcb-a48c-dcd17e8bad3a
+md"
+# 01c. Land-Sea Mask Filtering
+"
+
+# ╔═╡ 75ea588a-64df-4b31-9dae-f99f425ee55a
+md"
+### A. Loading Land-Sea Mask Data
+"
+
+# ╔═╡ c557eeb4-fab4-4751-af4b-bea2115e85a9
+begin
+	ds  = NCDataset(datadir("reanalysis","era5-TRPx0.25-lsm-sfc.nc"))
+	lon = ds["longitude"][:]
+	lat = ds["latitude"][:]
+	lsm = ds["lsm"][:]
+	close(ds)
+	md"Loading land-sea mask for the Tropical Region ..."
+end
+
+# ╔═╡ dc6929b0-c95d-4c71-9a4c-cb9fa4b01e34
+begin
+	coast = readdlm(datadir("GLB-i.txt"),comments=true,comment_char='#')
+	x = coast[:,1]; y = coast[:,2];
+	md"Loading coastlines ..."
+end
+
+# ╔═╡ a9bae1a3-c994-4b36-b9f8-740aa14a3929
+begin
+	pplt.close(); f1,a1 = pplt.subplots(aspect=1,axwidth=3)
+	
+	c = a1[1].pcolormesh(lon,lat,lsm',levels=(0:10)/10,extend="both",cmap="Delta")
+	# a1[1].plot(x,y,c="k",lw=0.5)
+	a1[1].plot([95.5,106,105.5,95,95.5],[6.5,-6,-6.5,6,6.5],c="r",lw=1)
+	a1[1].plot([95,105.5,105,94.5,95],[6,-6.5,-7,5.5,6],c="r",lw=1)
+	a1[1].plot([94.5,105,104.5,94,94.5],[5.5,-7,-7.5,5,5.5],c="r",lw=1)
+	a1[1].plot([94,104.5,104,93.5,94],[5,-7.5,-8,4.5,5],c="r",lw=1)
+	a1[1].plot([93.5,104,103.5,93,93.5],[4.5,-8,-8.5,4,4.5],c="r",lw=1)
+	a1[1].plot([93,103.5,103,92.5,93],[4,-8.5,-9,3.5,4],c="r",lw=1)
+	a1[1].format(xlim=(90,110),ylim=(-10,10))
+	a1[1].colorbar(c,loc="r")
+	
+	f1.savefig("testlsm.png",transparent=false,dpi=200)
+	PNGFiles.load("testlsm.png")
+end
+
+# ╔═╡ 95ffc474-8416-451c-ba9a-4452dc582626
+md"
+### B. Smoothing the Land-Sea Mask?
+"
+
+# ╔═╡ a66e0d88-7f48-4d7d-b716-3a424d93e90b
+function filterlsm(olsm,iterations=1)
+	
+	it = 0
+	nlsm = deepcopy(olsm)
+	while it < iterations
+		nlsm = log10.(imfilter(10. .^nlsm, Kernel.gaussian(5),"circular"));
+		nlsm = (nlsm.+olsm)/2
+		it += 1
+	end
+	
+	nlsm[nlsm.<0] .= minimum(nlsm[nlsm.>0])
+	
+	return nlsm
+	
+end
+
+# ╔═╡ d3864cc7-284b-4278-9499-0b6f4d274608
+begin
+	nlsm = filterlsm(lsm,3)
+	md"Performing gaussian filtering/smoothing on land-sea mask ..."
+end
+
+# ╔═╡ 05c13f7e-58f8-4136-830d-fb20a9dc0e6f
+begin
+	geo = GeoRegion("AMZ")
+	md"Defining region coordinates ..."
+end
+
+# ╔═╡ 8471606c-fead-450a-80f0-fd4cf308c3e5
+begin
+	N,S,E,W = geo.N,geo.S,geo.E,geo.W
+	ggrd = RegionGrid(geo,lon,lat)
+	ilon = ggrd.ilon; nlon = length(ggrd.ilon)
+	ilat = ggrd.ilat; nlat = length(ggrd.ilat)
+	rlsm = zeros(nlon,nlat)
+	flsm = zeros(nlon,nlat)
+	if typeof(ggrd) <: PolyGrid
+		mask = ggrd.mask
+	else; mask = ones(nlon,nlat)
+	end
+	for glat in 1 : nlat, glon in 1 : nlon
+		rlsm[glon,glat] =  lsm[ilon[glon],ilat[glat]] * mask[glon,glat]
+		flsm[glon,glat] = nlsm[ilon[glon],ilat[glat]] * mask[glon,glat]
+	end
+	md"Extracting information for region ..."
+end
+
+# ╔═╡ 51d55158-32df-4135-ab12-186e295ce499
+begin
+	asp = (E-W+2)/(N-S+2)
+	if asp > 1.5
+		freg,areg = pplt.subplots(nrows=2,axwidth=asp*1.2,aspect=asp)
+	else
+		freg,areg = pplt.subplots(ncols=2,axwidth=2.5,aspect=asp)
+	end
+	
+	lvls = vcat(10. .^(-6:-1),0.2,0.5,0.8,0.9)
+
+	creg = areg[1].pcolormesh(
+		ggrd.glon,ggrd.glat,rlsm',
+		levels=lvls,cmap="delta",extend="both"
+	)
+	areg[1].plot(x,y,c="k",lw=0.5)
+	areg[1].format(urtitle="Raw")
+	
+	areg[2].pcolormesh(
+		ggrd.glon,ggrd.glat,flsm',
+		levels=lvls,cmap="delta",extend="both"
+	)
+	areg[2].plot(x,y,c="k",lw=0.5)
+	areg[2].format(urtitle="Filtered")
+	
+	freg.colorbar(creg,loc="r")
+
+	for ax in areg
+		ax.format(
+			xlim=(ggrd.glon[1].-1,ggrd.glon[end].+1),
+			xlabel=L"Longitude / $\degree$",
+			ylim=(S-1,N+1),ylabel=L"Latitude / $\degree$",
+			suptitle="Land-Sea Mask",
+			grid=true
+		)
+	end
+
+	freg.savefig(plotsdir("flsm_$(geo.regID).png"),transparent=false,dpi=200)
+	PNGFiles.load(plotsdir("flsm_$(geo.regID).png"))
+end
+
+# ╔═╡ Cell order:
+# ╟─faa41f45-5a58-4bcb-a48c-dcd17e8bad3a
+# ╟─a675084e-f638-11eb-2930-b70006b9445c
+# ╟─4e08778c-cd46-4a79-9b6a-96ca6447e975
+# ╟─75ea588a-64df-4b31-9dae-f99f425ee55a
+# ╟─c557eeb4-fab4-4751-af4b-bea2115e85a9
+# ╟─dc6929b0-c95d-4c71-9a4c-cb9fa4b01e34
+# ╟─a9bae1a3-c994-4b36-b9f8-740aa14a3929
+# ╟─95ffc474-8416-451c-ba9a-4452dc582626
+# ╠═a66e0d88-7f48-4d7d-b716-3a424d93e90b
+# ╟─d3864cc7-284b-4278-9499-0b6f4d274608
+# ╟─05c13f7e-58f8-4136-830d-fb20a9dc0e6f
+# ╟─8471606c-fead-450a-80f0-fd4cf308c3e5
+# ╟─51d55158-32df-4135-ab12-186e295ce499
---
+++ notebooks/09c-csfvprcpreg.jl
@@ -0,0 +1,214 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 858e2874-57e8-4e18-b4cc-7f65621ec1f5
+begin
+	using Pkg; Pkg.activate()
+	using DrWatson
+
+md"Using DrWatson in order to ensure reproducibility between different machines ..."
+end
+
+# ╔═╡ a39537b9-34d4-463e-904a-4e1b51992e94
+begin
+	@quickactivate "TroPrecLS"
+	using DelimitedFiles
+	using GeoRegions
+	using ImageFiltering
+	using NCDatasets
+	using Statistics
+
+	using ImageShow, PNGFiles
+	using PyCall, LaTeXStrings
+	pplt = pyimport("proplot")
+
+md"Loading modules for the TroPrecLS project..."
+end
+
+# ╔═╡ d9faa384-d455-11eb-32a2-1723c787fbc7
+md"
+# 09c. Column Saturation Fraction vs Precipitation Rate by Regino
+
+This is a more in-depth review of notebook `09b-csfvprcp.jl`, where we dive into each region, and explore the changes in the CRH vs Precipitation curve as we move from land to sea.
+"
+
+# ╔═╡ 29425799-b14a-4200-829b-619866a2024a
+md"
+### A. The Basic Breakdown over the Tropics
+"
+
+# ╔═╡ 22858627-a7f3-4f40-b343-75fad90c0e6e
+begin
+	dsg = NCDataset(datadir("reanalysis","gpm.jld2"))
+	gfq = dsg["freq"][:] * 1
+	gpm = dsg["prcp"][:] * 3600
+	csf = dsg["csf"][:]
+	close(dsg)
+	md"Loading GPM CSF vs Precipitation rate data ..."
+end
+
+# ╔═╡ 8414f79b-763c-489c-bfc7-cc04ad5aac41
+begin
+	dse = NCDataset(datadir("reanalysis","era5.jld2"))
+	efq = dse["freq"][:] * 1
+	era = dse["prcp"][:] * 1000
+	close(dse)
+	md"Loading ERA5 CSF vs Precipitation rate data ..."
+end
+
+# ╔═╡ bcc90c75-8c85-47da-8b1a-b829bfe2f93b
+begin
+	dsl = NCDataset(datadir("reanalysis","era5-TRPx0.25-lsm-sfc.nc"))
+	lon = dsl["longitude"][:] * 1
+	lat = dsl["latitude"][:] * 1
+	lsm = dsl["lsm"][:] * 1
+	close(dsl)
+	md"Loading land-sea mask data ..."
+end
+
+# ╔═╡ 73c5847f-9783-49dd-a7cb-736113fe75bf
+begin
+	lsc = pplt.Colors("Delta",15)
+	md"Colours for different regions ..."
+end
+
+# ╔═╡ 75e29873-02df-48fa-9223-9a888659c241
+md"
+### B. Filtering the Land-Sea Mask ...
+"
+
+# ╔═╡ b00fc20d-bfa6-4581-b166-19ae08bcba03
+function filterlsm(olsm,iterations=1)
+	
+	it = 0
+	nlsm = deepcopy(olsm)
+	while it < iterations
+		nlsm = log10.(imfilter(10. .^nlsm, Kernel.gaussian(5),"circular"));
+		nlsm = (nlsm.+olsm)/2
+		it += 1
+	end
+	
+	nlsm[nlsm.<0] .= minimum(nlsm[nlsm.>0])
+	
+	return nlsm
+	
+end
+
+# ╔═╡ c20a6e32-6be4-4e58-a1da-72d7c5fd7421
+begin
+	nlsm = filterlsm(lsm,3)
+	md"Performing gaussian filtering/smoothing on land-sea mask ..."
+end
+
+# ╔═╡ 91a53908-8090-4835-ac31-127919a175ac
+md"
+### B. Binning into different regions
+
+Now, we bin the data into the different sub-regions within the Tropics as was defined in the earlier notebook `01-domain.jl`.
+"
+
+# ╔═╡ 38ca34a7-772e-4e8c-b7a9-0ea9cdebae5b
+function extractocnlnd(geo,prc,frq,lon,lat,lsm)
+
+	ggrd = RegionGrid(geo,lon,lat)
+	ilon = ggrd.ilon; nlon = length(ggrd.ilon)
+	ilat = ggrd.ilat; nlat = length(ggrd.ilat)
+    rprc = zeros(nlon,nlat); ncsf = size(frq,3)
+    rfrq = zeros(nlon,nlat)
+	rlsm = zeros(nlon,nlat)
+	lsmb = vcat(0,10. .^(-6:-1),0.2,0.5,0.8,0.9,1); nlsb = length(lsmb)
+	cvp  = zeros(nlsb-1,ncsf)
+
+	if typeof(ggrd) <: PolyGrid
+		  mask = ggrd.mask
+	else; mask = ones(nlon,nlat)
+	end
+
+	for icsf = 1 : ncsf
+		for glat in 1 : nlat, glon in 1 : nlon
+			rprc[glon,glat] = prc[ilon[glon],ilat[glat],icsf]
+			rfrq[glon,glat] = frq[ilon[glon],ilat[glat],icsf]
+			rlsm[glon,glat] = lsm[ilon[glon],ilat[glat]] * mask[glon,glat]
+		end
+		for ilsb = 1 : (nlsb-1)
+			cvp[ilsb,icsf] = sum(rprc[(rlsm.>=lsmb[ilsb]).&(rlsm.<=lsmb[ilsb+1])]) ./ 
+							 sum(rfrq[(rlsm.>=lsmb[ilsb]).&(rlsm.<=lsmb[ilsb+1])])
+		end
+	end
+
+	return cvp
+
+end
+
+# ╔═╡ 1cd2bee0-0e9a-4c82-afaf-f54dbe4a359a
+function normbin(bindata)
+	return bindata / sum(bindata) * length(bindata)
+end
+
+# ╔═╡ d4ea3167-6fd2-401e-965a-81163915136e
+geo = GeoRegion("SEA")
+
+# ╔═╡ 48e74e01-c1ef-456e-9e6c-5f6da0ce93bf
+begin
+	gcvp = extractocnlnd(geo,gpm,gfq,lon,lat,lsm)
+	md"Extracting Regional Data for GPM data ..."
+end
+
+# ╔═╡ 35c157f3-fdcc-4259-888d-ea6af3aa362a
+begin
+	ecvp = extractocnlnd(geo,era,efq,lon,lat,lsm)
+	md"Extracting Regional Data for ERA5 data ..."
+end
+
+# ╔═╡ d0aebfcd-6068-4ff0-9aae-fef936be7a99
+begin
+	pplt.close(); f2,a2 = pplt.subplots(ncols=2,aspect=2,axwidth=3)
+
+	for ilsb = 1 : 11
+		a2[1].plot(csf,gcvp[ilsb,:],lw=1,c=lsc[ilsb+2])
+		a2[2].plot(csf,ecvp[ilsb,:],lw=1,c=lsc[ilsb+2])
+	end
+	c = a2[1].contourf(
+		[20,40],[0.1,1],ones(2,2)*NaN,
+		levels=vcat(10. .^(-6:-1),0.2,0.5,0.8,0.9),
+		cmap=cmap="delta",extend="both"
+	)
+	a2[1].format(ltitle="(a) GPM Precipitation")
+	a2[2].format(ltitle="(b) ERA5 Precipitation")
+	f2.colorbar(c,loc="b",label="Land-Sea Mask Value")
+
+	for ax in a2
+		ax.format(
+			xlim=(0,100),xlabel="Column Relative Humidity",
+			ylim=10. .^(-3.5,1.5),ylabel=L"Precipitation Rate / mm hr$^{-1}$",
+			yscale="log",ylocator=10. .^(-3:1),
+			suptitle="Column Relative Humidity vs Precipitation Rate | $(geo.name) ($(geo.regID))"
+		)
+	end
+
+	f2.savefig(plotsdir("csfvprcp-$(geo.regID).png"),transparent=false,dpi=200)
+	PNGFiles.load(plotsdir("csfvprcp-$(geo.regID).png"))
+end
+
+# ╔═╡ Cell order:
+# ╠═d9faa384-d455-11eb-32a2-1723c787fbc7
+# ╟─858e2874-57e8-4e18-b4cc-7f65621ec1f5
+# ╟─a39537b9-34d4-463e-904a-4e1b51992e94
+# ╟─29425799-b14a-4200-829b-619866a2024a
+# ╟─22858627-a7f3-4f40-b343-75fad90c0e6e
+# ╟─8414f79b-763c-489c-bfc7-cc04ad5aac41
+# ╟─bcc90c75-8c85-47da-8b1a-b829bfe2f93b
+# ╟─73c5847f-9783-49dd-a7cb-736113fe75bf
+# ╠═75e29873-02df-48fa-9223-9a888659c241
+# ╠═b00fc20d-bfa6-4581-b166-19ae08bcba03
+# ╟─c20a6e32-6be4-4e58-a1da-72d7c5fd7421
+# ╟─91a53908-8090-4835-ac31-127919a175ac
+# ╟─38ca34a7-772e-4e8c-b7a9-0ea9cdebae5b
+# ╟─1cd2bee0-0e9a-4c82-afaf-f54dbe4a359a
+# ╠═d4ea3167-6fd2-401e-965a-81163915136e
+# ╟─48e74e01-c1ef-456e-9e6c-5f6da0ce93bf
+# ╟─35c157f3-fdcc-4259-888d-ea6af3aa362a
+# ╟─d0aebfcd-6068-4ff0-9aae-fef936be7a99
