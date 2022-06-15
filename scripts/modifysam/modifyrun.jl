7549278211623a17bdf59da49fcd8aba9ad1c192
Nathanael Wong <natgeo.wong@outlook.com>
2022-06-14 21:52:06 -0400

    Added modifyrun.jl to modify modelrun and ensemblexx scripts

--- scripts/modifysam/modifyrun.jl
+++ scripts/modifysam/modifyrun.jl
@@ -1,26 +1,48 @@
 using DrWatson
-@quickactivate "ExploreWTGSpace"
+@quickactivate "TroPrecLS"
 using Printf
 
 sdlist = [0.05,0.07071,0.1,0.1414,0.2,0.3162]
-sdlist = vcat(sdlist,sdlist*10,sdlist*100,50)
+sdlist = vcat(sdlist,sdlist*10,sdlist*100)
+pop!(sdlist)
 
 for sd in sdlist
+
     sdstr = string(round(sd,sigdigits=4))
     expii = "Slab$(@sprintf("%05.2f",sd))"
     expii = replace(expii,"."=>"d")
+
     for crhii = 20 : 5 : 95
-        trun  = projectdir("run",expii,crhii,"ensemblexx.sh")
+
+        trun  = projectdir("run",expii,"ensemblexx.sh")
         open(trun,"r") do frun
             s = read(frun,String)
             for imember = 1 : 5
                 memstr = @sprintf("%02d",imember)
-                nrun = projectdir("run",expii,crhii,"ensemble$(memstr).sh")
+                nrun = projectdir("run",expii,"crh$(crhii)_ensemble$(memstr).sh")
                 open(nrun,"w") do wrun
-                    sn = replace(s,"=member00"=>"=member$(memstr)")
+                    sn = replace(s,"[crh]"=>"[$(crhii)]")
+                    sn = replace(s,"Slab[ssdss]"=>"$(expii)")
+                    sn = replace(s,"member[xx]"=>"=member$(memstr)")
                     write(wrun,sn)
                 end
             end
         end
+
+        trun  = projectdir("run",expii,"modelrun.sh")
+        open(trun,"r") do frun
+            s = read(frun,String)
+            for imember = 1 : 5
+                memstr = @sprintf("%02d",imember)
+                nrun = projectdir("run",expii,"crh$(crhii).sh")
+                open(nrun,"w") do wrun
+                    sn = replace(s,"[crh]"=>"[$(crhii)]")
+                    sn = replace(s,"Slab[ssdss]"=>"$(expii)")
+                    write(wrun,sn)
+                end
+            end
+        end
+
     end
+
 end
