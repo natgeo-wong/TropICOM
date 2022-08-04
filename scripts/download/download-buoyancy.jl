42d5b7374f304b22ed8e782111a3196873b55753
Nathanael Wong <natgeo.wong@outlook.com>
2022-08-03 22:41:20 -0400

    Debug downloads

--- scripts/download/download-buoyancy.jl
+++ scripts/download/download-buoyancy.jl
@@ -7,7 +7,7 @@ e5ds = ERA5Monthly(dtbeg=Date(1979),dtend=Date(2021),eroot=datadir())
 egeo = ERA5Region(GeoRegion("GLB"),gres=0.25)
 
 esgl = [SingleVariable("t2m"),SingleVariable("skt"),SingleVariable("d2m")]
-epre = [PressureVariable("t",hPa=0),PressureVariable("q",hPa=0)]
+epre = [PressureVariable("t",hPa=0,throw=false),PressureVariable("q",hPa=0,throw=false)]
 
 for evar in esgl
     download(e5ds,evar,egeo)
