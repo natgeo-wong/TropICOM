0ce73f4050fd90923451db81a037e090c7be9ff0
Nathanael Wong <natgeo.wong@outlook.com>
2022-08-04 14:46:23 -0400

    Adding geopotential height as an additional variable of interest

--- scripts/download/download-buoyancy.jl
+++ scripts/download/download-buoyancy.jl
@@ -7,7 +7,10 @@ e5ds = ERA5Monthly(dtbeg=Date(1979),dtend=Date(2021),eroot=datadir())
 egeo = ERA5Region(GeoRegion("GLB"),gres=0.25)
 
 esgl = [SingleVariable("t2m"),SingleVariable("skt"),SingleVariable("d2m")]
-epre = [PressureVariable("t",hPa=0,throw=false),PressureVariable("q",hPa=0,throw=false)]
+epre = [
+    PressureVariable("t",hPa=0,throw=false),PressureVariable("q",hPa=0,throw=false),
+    PressureVariable("z",hPa=0,throw=false)
+]
 
 for evar in esgl
     download(e5ds,evar,egeo)
