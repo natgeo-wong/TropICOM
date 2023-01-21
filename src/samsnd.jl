2af77e482625f1c7e5f8a666af4ec7dab8a6f2d4
Nathanael Wong <natgeo.wong@outlook.com>
2023-01-21 13:27:57 -0500

    Changing some notebooks

--- src/samsnd.jl
+++ src/samsnd.jl
@@ -4,13 +4,14 @@ using Printf
 using Statistics
 
 function createsndmean(
-    sndname::AbstractString;
-    exp::AbstractString, config::AbstractString,
+    sndname :: AbstractString;
+    expname :: AbstractString,
+    config  :: AbstractString,
     ndays::Integer=100, psfc::Real, extract=false
 )
 
     mkpath(projectdir("exp/snd")); fsnd = projectdir("exp/snd/$(sndname)")
-    statds = NCDataset(datadir("$exp/$config/OUT_STAT/RCE_TroPrecLS-$exp.nc"))
+    statds = NCDataset(datadir("$expname/OUT_STAT/RCE_TroPrecLS-$expname-$config.nc"))
 
     nz = statds.dim["z"]; nt = statds.dim["time"]
     z  = statds["z"][:]
@@ -47,43 +48,43 @@ function createsndmean(
 
 end
 
-function createsnddiurnal(
-    sndname::AbstractString;
-    exp::AbstractString, config::AbstractString,
-    ndays::Integer=100, psfc::Real, extract=false
-)
-    mkpath(projectdir("exp/snd")); fsnd = projectdir("exp/snd/$(sndname)")
-    statds = NCDataset(datadir("$exp/$config/OUT_STAT/RCE_TroPrecLS-$exp.nc"))
+# function createsnddiurnal(
+#     sndname::AbstractString;
+#     exp::AbstractString, config::AbstractString,
+#     ndays::Integer=100, psfc::Real, extract=false
+# )
+#     mkpath(projectdir("exp/snd")); fsnd = projectdir("exp/snd/$(sndname)")
+#     statds = NCDataset(datadir("$exp/$config/OUT_STAT/RCE_ExploreWTGSpace-$exp.nc"))
 
-    nz = statds.dim["z"]; nt = statds.dim["time"]; p = statds["p"][:]
-    dt = (statds["time"][end] - statds["time"][1]) / (nt-1)
-    dystep = round(Int,1/dt); beg = ndays*dystep-1
-    dbeg = Int(floor(statds["time"][1]))
-    dend = Int(floor(statds["time"][end]))
+#     nz = statds.dim["z"]; nt = statds.dim["time"]; p = statds["p"][:]
+#     dt = (statds["time"][end] - statds["time"][1]) / (nt-1)
+#     dystep = round(Int,1/dt); beg = ndays*dystep-1
+#     dbeg = Int(floor(statds["time"][1]))
+#     dend = Int(floor(statds["time"][end]))
 
-    snddata = zeros(nz,6,dystep);
-    snddata[:,1,:] .= statds["z"][:]; snddata[:,2,:] .= -999.0
+#     snddata = zeros(nz,6,dystep);
+#     snddata[:,1,:] .= statds["z"][:]; snddata[:,2,:] .= -999.0
 
-    tmp = mod.(statds["time"][(end-beg):end],1)
-    raw = dropdims(mean(reshape(tmp,dystep,ndays),dims=2),dims=2)
-    ind = sortperm(raw); t = raw[ind]
+#     tmp = mod.(statds["time"][(end-beg):end],1)
+#     raw = dropdims(mean(reshape(tmp,dystep,ndays),dims=2),dims=2)
+#     ind = sortperm(raw); t = raw[ind]
 
-    tmp  = zeros(nz,ndays*dystep)
-    tmp .= statds["THETA"][:,(end-beg):end]
-    raw  = dropdims(mean(reshape(tmp,nz,dystep,ndays),dims=3),dims=3)
-    snddata[:,3,:] .= raw[:,ind]
+#     tmp  = zeros(nz,ndays*dystep)
+#     tmp .= statds["THETA"][:,(end-beg):end]
+#     raw  = dropdims(mean(reshape(tmp,nz,dystep,ndays),dims=3),dims=3)
+#     snddata[:,3,:] .= raw[:,ind]
 
-    tmp .= statds["QT"][:,(end-beg):end];
-    raw  = dropdims(mean(reshape(tmp,nz,dystep,ndays),dims=3),dims=3)
-    snddata[:,4,:] .= raw[:,ind]
+#     tmp .= statds["QT"][:,(end-beg):end];
+#     raw  = dropdims(mean(reshape(tmp,nz,dystep,ndays),dims=3),dims=3)
+#     snddata[:,4,:] .= raw[:,ind]
 
-    close(statds)
+#     close(statds)
 
-    printsnd(fsnd,snddata,psfc,t,[dbeg,dend])
+#     printsnd(fsnd,snddata,psfc,t,[dbeg,dend])
 
-    if extract; return t,p,snddata end
+#     if extract; return t,p,snddata end
 
-end
+# end
 
 function printsnd(fsnd::AbstractString, snddata::Array{<:Real,2}, p::Real)
 
