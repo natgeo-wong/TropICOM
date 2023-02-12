7815190c16fc47280ffae700f3d882cf501276bb
Nathanael Wong <natgeo.wong@outlook.com>
2023-02-11 19:21:27 -0500

    Managing projects and other stuff

--- src/SimpleIslandModels/src/SimpleIslandModels.jl
+++ src/SimpleIslandModels/src/SimpleIslandModels.jl
@@ -8,10 +8,39 @@ using Printf
 using Statistics
 
 ## Modules Used
-using ProgressLogging
+using ProgressMeter
 
 import Base: eltype, show, run
 
+export
+        CreateFixedSSTModel, CreateFixedAtmosModel, CreateMixedLayerModel
+
+"""
+    SimpleIslandModel
+
+Abstract supertype for different models that YangShallowWater can run.
+"""
+abstract type SimpleIslandModel end
+
+struct Variables{FT<:Real}
+    t :: Vector{FT}
+   S₀ :: Vector{FT}
+   Tₛ :: Vector{FT}
+   Tₐ :: Vector{FT}
+   temp :: Vector{FT} # temp = [t,S₀,Tₛ,Tₐ,δt]
+   stat :: Vector{FT} # temp = [t,S₀,Tₛ,Tₐ,δt]
+end
+
+const σ = 5.670374419e-8
+
 modulelog() = "$(now()) - SimpleIslandModels.jl"
+
+include("FixedAtmos.jl")
+include("FixedSST.jl")
+include("MixedLayer.jl")
+
+include("variables.jl")
+include("calculate.jl")
+include("run.jl")
 
 end # module SimpleIslandModels
